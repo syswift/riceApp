192d676b7d828cf6146975ecf133337dc9b1848d
zzchen <2236631577@qq.com>
2021-02-07 16:02:24 +0800

    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   version: '10.4.0',
   note: '商品列表页面增加触底加载下一页', // 这个为版本描述，无需修改
-  subDomain: 'tz', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
+  subDomain: '	d780cfe9cba3b742dda88d755cd66aba', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
   sdkAppID: 1400450467, // 腾讯实时音视频应用编号，请看教程 https://www.it120.cc/help/nxoqsl.html
 }
